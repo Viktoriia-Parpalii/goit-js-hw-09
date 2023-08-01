a0697a8f90fcf5a828a48573a08eed13c4060480
Viktoriia-Parpalii <127688031+Viktoriia-Parpalii@users.noreply.github.com>
2023-08-01 21:02:04 +0300

    Completed task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,28 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
-const NOTIFICATION_DELAY = 1000;
+const INETRVAL_DELAY = 1000;
 let timerId = null;
 
 startBtn.addEventListener('click', changeColor);
 stopBtn.addEventListener('click', stopChangeColor);
 
-function changeColor() {
-  document.body.style.backgroundColor = getRandomHexColor();
-  startBtn.setAttribute('disabled', '');
-  stopBtn.removeAttribute('disabled', '');
-  timerId = setInterval(changeColor, NOTIFICATION_DELAY);
-}
-// if (!startBtn.matches('[disabled]'))
-
-function stopChangeColor() {
-  clearInterval(timerId);
-  stopBtn.setAttribute('disabled', '');
-  startBtn.removeAttribute('disabled', '');
-}
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function changeColor() {
+  timerId = setInterval(
+    () => (document.body.style.backgroundColor = getRandomHexColor()),
+    INETRVAL_DELAY
+  );
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function stopChangeColor() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
 }
