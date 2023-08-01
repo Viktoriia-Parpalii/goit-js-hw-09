30d4b00f796e59e005de754a8444140afa440845
Viktoriia-Parpalii <127688031+Viktoriia-Parpalii@users.noreply.github.com>
2023-08-01 19:33:26 +0300

    Added task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const NOTIFICATION_DELAY = 1000;
+let timerId = null;
 
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', stopChangeColor);
+
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled', '');
+}
+if (!startBtn.matches('[disabled]'))
+  timerId = setInterval(changeColor, NOTIFICATION_DELAY);
+
+function stopChangeColor() {
+  clearInterval(timerId);
+  stopBtn.setAttribute('disabled', '');
+  startBtn.removeAttribute('disabled', '');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
